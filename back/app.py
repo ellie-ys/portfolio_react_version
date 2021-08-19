ad339149f58b701dd16dfd05b80736ddf535200e
[2기-1반]최윤성 <yunsung@ewhain.net>
2021-08-20 00:04:09 +0900

    DB와 연결, 데이터 읽고 쓰기 연습

--- back/app.py
+++ back/app.py
@@ -1,24 +1,43 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask, Blueprint
 from db_connect import db
 import config
+from admin import admin_password
+from models.user import User
+
 
 def create_app():
-
     app = Flask(__name__)
-
     app.config.from_object(config) 
+
     db.init_app(app)
 
-    from blueprints import main_view
-    app.register_blueprint(main_view.bp)
+
+    @app.route("/api")
+    def info():
+        
+        return "Hello World!"
 
 
-    app.secret_key = 'Top Secret!'
+    @app.route('/register')
+    def index():
+
+        user1 = User("a@elice.io", "1234", "racer1")
+        db.session.add(user1)
+        user2 = User("b@elice.io", "1234", "racer2")
+        db.session.add(user2)
+        user3 = User("c@elice.io", "1234", "racer3")
+        db.session.add(user3)
+
+        db.session.commit()
+
+        return "SUCCESS"
+
+    app.secret_key = admin_password
     app.config['SESSION_TYPE'] = 'filesystem'
-
 
     return app
 
+
 if __name__ == "__main__":
-    create_app().run(debug=True, port=1234)
+    create_app().run(debug=True)
--- back/models/education.py
+++ back/models/education.py
@@ -1,17 +1,17 @@
 from db_connect import db
 
 
-class elice_education(db.Model):
+class Education(db.Model):
 
-    __tablename__ = 'elice_education'
+    __tablename__ = 'education'
 
     edu_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
-    school = db.Column(db.String(255), nullable = False)
-    major = db.Column(db.String(255),  nullable = False)
-    type = db.Column(db.String(255), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('elice_user.id'), nullable=False)
+    school = db.Column(db.String(45), nullable = False)
+    major = db.Column(db.String(45),  nullable = False)
+    degree = db.Column(db.String(45), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
 
-    def __init__(self, school, major, type):
+    def __init__(self, school, major, degree):
         self.school = school
         self.major = major
-        self.type = type
+        self.degree = degree
