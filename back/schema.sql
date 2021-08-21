2db4d19a1d457c6a33e0c21d0afd13febe944701
[2기-1반]최윤성 <yunsung@ewhain.net>
2021-08-21 20:31:23 +0900

    DB user table의 password 크기 (100-> 255)로 변경

--- back/schema.sql
+++ back/schema.sql
@@ -4,7 +4,7 @@ USE elice;
 CREATE TABLE user(
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
     `name` VARCHAR(45) NOT NULL,
     `description` VARCHAR(255),
     `image` VARCHAR(255)
