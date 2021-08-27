174aeda2d7085d49a949f7d270babc1415f992a1
[2기-1반]최윤성 <yunsung@ewhain.net>
2021-08-28 00:27:53 +0900

    또 수정

--- front_2/src/components/port.jsx
+++ front_2/src/components/port.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import axios from "axios";
 
 const Portfolio = () => {
-  async function getUserInfo(e) {
-    e.preventDefault();
-    const response = await axios.get(`http://127.0.0.1:5000/elicer/${user}`);
-    if (response.data.result === "success") user = response.data.data;
-    return user;
-  }
+  // async function getUserInfo(e) {
+  //   e.preventDefault();
+  //   const response = await axios.get(`http://127.0.0.1:5000/elicer/${user}`);
+  //   if (response.data.result === "success") user = response.data.data;
+  //   return user;
+
   return (
-    <div data-id={user.id}>
+    <div>
       <div>
-        <span>{user.name}</span>
-        <p>{user.description ? user.description : "한 줄 소개"}</p>
+        <span>이름</span>
+        <p>한 줄 소개</p>
       </div>
     </div>
   );
