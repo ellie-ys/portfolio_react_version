880f6fb04e3025c8aa4b3b304a7955fa43038a90
[2기-1반]최윤성 <yunsung@ewhain.net>
2021-08-28 22:40:13 +0900

    다시server

--- back/config.py
+++ back/config.py
@@ -1,4 +1,4 @@
 from admin import db_password
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'+db_password+'@localhost:3306/elice?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'+db_password+'@localhost:3306/elice'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
