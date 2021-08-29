674c52b2cd22fb26f0f3fb7edad6975491b72418
[2기-1반]최윤성 <yunsung@ewhain.net>
2021-08-30 01:16:55 +0900

    axios url - SERVER 도메인으로 변경

--- front_2/src/apis/elicer.js
+++ front_2/src/apis/elicer.js
@@ -1,29 +1,41 @@
 import axios from "axios";
 
 export const getUserRequest = async (userId) => {
-  const response = await axios.get("/api/elicer/${userId}");
+  const response = await axios.get(
+    "http://kdt-1st-project-77.koreacentral.cloudapp.azure.com/api/elicer/${userId}"
+  );
   return response.data.user;
 };
 
 export const updateUserRequest = async (userId, name, description) => {
-  await axios.patch("/api/elicer/${userId}", {
-    name,
-    description,
-  });
+  await axios.patch(
+    "http://kdt-1st-project-77.koreacentral.cloudapp.azure.com/api/elicer/${userId}",
+    {
+      name,
+      description,
+    }
+  );
 };
 
 export const updateProfileImage = async (userId, image) => {
-  await axios.patch("/api/elicer/${userId}/image", {
-    image,
-  });
+  await axios.patch(
+    "http://kdt-1st-project-77.koreacentral.cloudapp.azure.com/api/elicer/${userId}/image",
+    {
+      image,
+    }
+  );
 };
 
 export const networkUsers = async (query) => {
-  const response = await axios.get("/api/elicer?query=${query}");
+  const response = await axios.get(
+    "http://kdt-1st-project-77.koreacentral.cloudapp.azure.com/api/elicer?query=${query}"
+  );
   return response.data.users;
 };
 
 export const searchUsers = async (query) => {
-  const response = await axios.get("/api/elicer?query=${query}");
+  const response = await axios.get(
+    "http://kdt-1st-project-77.koreacentral.cloudapp.azure.com/api/elicer?query=${query}"
+  );
   return response.data.users;
 };
--- front_2/src/apis/portfolio.js
+++ front_2/src/apis/portfolio.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchPortfolio = async (userId) => {
   const response = await axios.get(
-    "/api/portfolio/${userId}"
+    "http://kdt-1st-project-77.koreacentral.cloudapp.azure.com/api/portfolio/${userId}"
   );
   return response.data.portfolio;
 };
