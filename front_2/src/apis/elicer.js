1426b91ba33f2a45e7687b25df8365b89e6100c9
[2기-1반]최윤성 <yunsung@ewhain.net>
2021-08-30 15:25:42 +0900

    될까?

--- front_2/src/apis/elicer.js
+++ front_2/src/apis/elicer.js
@@ -1,29 +1,35 @@
 import axios from "axios";
 
 export const getUserRequest = async (userId) => {
-  const response = await axios.get(`/elicer/${userId}`);
+  const response = await axios.get(
+    `http://localhost:5000/api/elicer/${userId}`
+  );
   return response.data.user;
 };
 
 export const updateUserRequest = async (userId, name, description) => {
-  await axios.patch(`/elicer/${userId}`, {
+  await axios.patch(`http://localhost:5000/api/elicer/${userId}`, {
     name,
     description,
   });
 };
 
 export const updateProfileImage = async (userId, image) => {
-  await axios.patch(`/elicer/${userId}/image`, {
+  await axios.patch(`http://localhost:5000/api/elicer/${userId}/image`, {
     image,
   });
 };
 
 export const networkUsers = async (query) => {
-  const response = await axios.get(`/elicer?query=${query}`);
+  const response = await axios.get(
+    `http://localhost:5000/api/elicer?query=${query}`
+  );
   return response.data.users;
 };
 
 export const searchUsers = async (query) => {
-  const response = await axios.get(`/elicer?query=${query}`);
+  const response = await axios.get(
+    `http://localhost:5000/api/elicer?query=${query}`
+  );
   return response.data.users;
 };
--- front_2/src/apis/portfolio.js
+++ front_2/src/apis/portfolio.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const fetchPortfolio = async (userId) => {
-  const response = await axios.get(`/portfolio/${userId}`);
+  const response = await axios.get(
+    `http://localhost:5000/api/portfolio/${userId}`
+  );
   return response.data.portfolio;
 };
