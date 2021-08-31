47c0373e6c13becc02bbc9a8f763835bd162c548
[2기-1반]최윤성 <yunsung@ewhain.net>
2021-08-31 12:32:38 +0900

    axios주소 수정

--- front_2/src/apis/auth.js
+++ front_2/src/apis/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const loginRequest = async (email, password) => {
-  const response = await axios.post(`/api/auth/login`, {
+  const response = await axios.post(`http://localhost:5000/api/auth/login`, {
     email,
     password,
   });
@@ -11,12 +11,12 @@ export const loginRequest = async (email, password) => {
 };
 
 export const logoutRequest = async () => {
-  await axios.get(`/api/auth/logout`);
+  await axios.get(`http://localhost:5000/api/auth/logout`);
   window.sessionStorage.clear();
 };
 
 export const registerRequest = async (email, password, name) => {
-  await axios.post(`/api/auth/register`, {
+  await axios.post(`http://localhost:5000/api/auth/register`, {
     email,
     password,
     name,
--- front_2/src/apis/elicer.js
+++ front_2/src/apis/elicer.js
@@ -1,35 +1,29 @@
 import axios from "axios";
 
 export const getUserRequest = async (userId) => {
-  const response = await axios.get(
-    `/api/elicer/${userId}`
-  );
+  const response = await axios.get(`/api/elicer/${userId}`);
   return response.data.user;
 };
 
 export const updateUserRequest = async (userId, name, description) => {
-  await axios.patch(`/api/elicer/${userId}`, {
+  await axios.patch(`/elicer/${userId}`, {
     name,
     description,
   });
 };
 
 export const updateProfileImage = async (userId, image) => {
-  await axios.patch(`/api/elicer/${userId}/image`, {
+  await axios.patch(`/elicer/${userId}/image`, {
     image,
   });
 };
 
 export const networkUsers = async (query) => {
-  const response = await axios.get(
-    `/api/elicer?query=${query}`
-  );
+  const response = await axios.get(`http://localhost:5000/api/elicer?query=${query}`);
   return response.data.users;
 };
 
 export const searchUsers = async (query) => {
-  const response = await axios.get(
-    `/api/elicer?query=${query}`
-  );
+  const response = await axios.get(`http://localhost:5000/api/elicer?query=${query}`);
   return response.data.users;
 };
