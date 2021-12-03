9a4f52b5bb871017751281b60f360a47d259f6f4
Ellie-YS <dev.ellie7@gmail.com>
2021-12-03 17:40:10 +0900

    211203 [Fix] type, variable

--- front_2/src/portfolio/register/RegisterForm.jsx
+++ front_2/src/portfolio/register/RegisterForm.jsx
@@ -1,43 +1,61 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../../env";
 
 const RegisterForm = () => {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
-  const [pwCheck, setPwCheck] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPw, setVerifyPw] = useState("");
   const [userName, setUserName] = useState("");
 
   const history = useHistory();
+
+  const registerRequest = async () => {
+    const response = await axios.post(BACKEND_URL + "/register", {
+      email: email,
+      password: verifyPw,
+      name: userName,
+      type: 1,
+    });
+
+    console.count(response);
+  };
+
   const registerHandler = () => {
+    registerRequest();
     history.push("/login");
   };
   return (
     <>
       <form>
-        <p>아이디</p>
-        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+        <p>이메일</p>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <p>비밀번호</p>
         <input
           type="password"
-          value={pw}
-          onChange={(e) => setPw(e.target.value)}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <p>비밀번호 확인</p>
         <input
           type="password"
-          value={pwCheck}
-          onChange={(e) => setPwCheck(e.target.value)}
+          value={verifyPw}
+          onChange={(e) => setVerifyPw(e.target.value)}
         />
         <p>이름</p>
         <input
-          type="password"
+          type="text"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
         />
       </form>
       <button type="submit" onClick={registerHandler}>
-        {" "}
-        회원가입{" "}
+        회원가입
       </button>
     </>
   );
