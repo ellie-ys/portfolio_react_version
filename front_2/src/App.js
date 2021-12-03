8574162d17839706cb7ac3c47bf53627428fe5e5
Ellie-YS <dev.ellie7@gmail.com>
2021-12-03 13:09:07 +0900

    211203 Router 설정

--- front_2/src/App.js
+++ front_2/src/App.js
@@ -1,40 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import LoginForm from "./components/login";
-import RegisterForm from "./components/register";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import Portfolio from "./components/port";
+import { NavComponent, Main, Network, Login } from "./portfolio/components";
+import { Provider, useSelector } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <LoginForm />
+    <Provider store={store}>
+      <Router>
+        <NavComponent />
+        <Route path="/" exact>
+          <Main />
         </Route>
-
-        <Route exact path="/login">
-          <LoginForm />
-        </Route>
-
-        <Route exact path="/register">
-          <RegisterForm />
-        </Route>
-
-        <Route path="/elice">
-          <Portfolio />
-        </Route>
-
         <Route path="/network">
-          <Portfolio />
+          <Network />
         </Route>
-      </Switch>
-
-      <Footer />
-    </Router>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Router>
+    </Provider>
   );
 }
 
