11465262d57355d2fe27de62ad1d776e81f68a1e
Ellie-YS <dev.ellie7@gmail.com>
2021-12-03 23:07:08 +0900

    211203 [Add] : portfolio elements export

---
+++ front_2/src/portfolio/Contents.jsx
@@ -0,0 +1,5 @@
+export { default as Award } from "./contents/Award";
+export { default as Certificate } from "./contents/Certificate";
+export { default as Edu } from "./contents/Edu";
+export { default as Profile } from "./contents/Profile";
+export { default as Project } from "./contents/Project";
