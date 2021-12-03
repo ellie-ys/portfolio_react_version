7aeb2c0e0b77fd14208e97ffdca68fad8c0be525
Ellie-YS <dev.ellie7@gmail.com>
2021-12-03 13:06:21 +0900

    211203 components 모음

---
+++ front_2/src/portfolio/components.jsx
@@ -0,0 +1,6 @@
+export { default as Main } from "./Main";
+export { default as NavComponent } from "./Nav";
+export { default as Network } from "./Network";
+export { default as Posts } from "./Posts";
+export { default as SearchBar } from "./SearchBar";
+export { default as Login } from "./login/Login";
