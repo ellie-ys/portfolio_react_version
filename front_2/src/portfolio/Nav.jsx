c85e012e2a1ea27a0df561ebb9610afcdccd3a5c
Ellie-YS <dev.ellie7@gmail.com>
2021-12-03 15:44:05 +0900

    211203 Nav bar update

--- front_2/src/portfolio/Nav.jsx
+++ front_2/src/portfolio/Nav.jsx
@@ -2,39 +2,42 @@ import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../redux/action";
 
 const Navi = (props) => {
-  const dispatch = useDispatch();
-  const logoutHandler = () => {
-    dispatch(logout());
-  };
+    const dispatch = useDispatch();
+    const isLogin = useSelector((state) => state.auth);
 
-  return (
+    const logoutHandler = () => {
+        dispatch(logout());
+    };
+
+    return (
     <Navbar bg="light" expand="lg">
-      <Container>
+        <Container>
         <Navbar.Brand> Elice Racer </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Link to="/" className="nav-link">
-              메인
+                Main
             </Link>
             <Link to="/network" className="nav-link">
-              네트워크
+                Network
             </Link>
             <Link className="nav-link" onClick={logoutHandler}>
-              로그아웃
+            Logout
             </Link>
-            <Link to="/login" className="nav-link">
-              로그인
-            </Link>
-          </Nav>
+            {isLogin ?
+            (<Link className="nav-link" onClick={logoutHandler}>Logout</Link>) :
+            (<Link to="/login" className="nav-link">Login</Link>)}
+
+            </Nav>
         </Navbar.Collapse>
-      </Container>
+        </Container>
     </Navbar>
-  );
+    );
 };
 
 export default Navi;
