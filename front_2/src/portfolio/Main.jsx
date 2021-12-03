e2c0ac05b5d3a03fdca8f457d1fac6c24d086e67
Ellie-YS <dev.ellie7@gmail.com>
2021-12-03 15:35:10 +0900

    211203 Main.jsx 에 포트폴리오 구성 컴포넌트 추가

--- front_2/src/portfolio/Main.jsx
+++ front_2/src/portfolio/Main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Award, Certificate, Edu, Profile, Project } from "./contents";
 
 const Main = () => {
   const isLogin = useSelector((state) => state.auth);
   console.log(isLogin);
-  return <h1> Main </h1>;
+  return (
+    <>
+      <h1> Main </h1>
+      <div>
+        <Profile />
+        <Edu />
+        <Award />
+        <Project />
+        <Profile />
+        <Certificate />
+      </div>
+    </>
+  );
 };
 
 export default Main;
