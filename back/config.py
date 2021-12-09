c30c8d57444f1601c3087a2b5678e2f493d1e032
Ellie-YS <dev.ellie7@gmail.com>
2021-12-09 13:42:32 +0900

    211209 [Fix] Server: edit expires_acess 30DAYS -> 1 HOURS

--- back/config.py
+++ back/config.py
@@ -5,5 +5,5 @@ from datetime import timedelta
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'+db_password+'@localhost:3306/elice?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-expires_access = timedelta(days=30)
+expires_access = timedelta(hours=1)
 expires_refresh = timedelta(days=30)
