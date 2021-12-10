1bd75bb228350e00c9423992b48569f9090a9485
Ellie-YS <dev.ellie7@gmail.com>
2021-12-10 09:51:37 +0900

    211210 [Fix] Client : Add signup validation

--- front_2/src/portfolio/register/RegisterForm.jsx
+++ front_2/src/portfolio/register/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { BACKEND_URL } from "utils/env";
@@ -9,53 +9,78 @@ const RegisterForm = () => {
   const [verifyPw, setVerifyPw] = useState("");
   const [userName, setUserName] = useState("");
 
+  const [wrongPw, setWrongPw] = useState(true);
+  const [isBlank, setIsBlank] = useState(true);
+  const [allOk, setAllOk] = useState(false);
+
   const history = useHistory();
 
-  const registerRequest = async () => {
-    const response = await axios.post(BACKEND_URL + "/register", {
-      email: email,
-      password: verifyPw,
-      name: userName,
-      type: 1,
-    });
-
-    console.count(response);
+  const registerHandler = async () => {
+    if (allOk) {
+      try {
+        const response = await axios.post(BACKEND_URL + "/register", {
+          email: email,
+          password: pw,
+          password_check: pwCheck,
+          name: userName,
+          user_type: 1,
+        });
+        history.push("/login");
+      } catch (error) {
+        console.log(error.response);
+      }
+    }
   };
+  useEffect(() => {
+    if (pw !== pwCheck) setWrongPw(true);
+    else setWrongPw(false);
+  }, [pw, pwCheck]);
 
-  const registerHandler = () => {
-    registerRequest();
-    history.push("/login");
-  };
+  useEffect(() => {
+    if (email === "" || pw === "" || pwCheck === "" || userName === "")
+      setIsBlank(true);
+    else setIsBlank(false);
+  }, [email, pw, pwCheck, userName]);
+
+  useEffect(() => {
+    if (!wrongPw && !isBlank) setAllOk(true);
+    else setAllOk(false);
+  }, [wrongPw, isBlank]);
+
   return (
     <>
       <form>
-        <p>이메일</p>
+        <p>E-mail</p>
         <input
           type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <p>비밀번호</p>
+        <p>Password</p>
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <p>비밀번호 확인</p>
+        <p>Password Confirm</p>
         <input
           type="password"
           value={verifyPw}
           onChange={(e) => setVerifyPw(e.target.value)}
         />
-        <p>이름</p>
+        {pwCheck !== "" && wrongPw && (
+          <p style={{ color: "red" }}> 비밀번호가 일치하지 않습니다. </p>
+        )}
+        <p>Name</p>
         <input
           type="text"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
         />
+        {isBlank && <p style={{ color: "red" }}> 모든 항목을 채워주세요. </p>}
       </form>
       <button type="submit" onClick={registerHandler}>
-        회원가입
+        Register
       </button>
     </>
   );
