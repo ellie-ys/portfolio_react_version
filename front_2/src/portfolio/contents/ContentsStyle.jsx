2727c2eff347f3fca1a4d9db8903552dbba808a8
Ellie-YS <dev.ellie7@gmail.com>
2021-12-20 23:41:57 +0900

    211220 [Fix] Client: Main CSS

--- front_2/src/portfolio/contents/ContentsStyle.jsx
+++ front_2/src/portfolio/contents/ContentsStyle.jsx
@@ -1,47 +1,50 @@
 import styled from "styled-components";
 
 export const ContentsStyle = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-    margin: 15px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  margin: 15px;
 
-    button{
-        width: 30%;
-        margin: 0 auto;
-    }
+  + div {
+    margin-top: 2rem;
+  }
+
+  button {
+    width: 30%;
+    margin: 0 auto;
+  }
 `;
 
 export const ContentsButtonWrapper = styled.span`
-    margin-top: 20px;
-    svg{
-        cursor: pointer;
-        float: right;
-        margin-right: 2rem;
-    }
-`
+  margin-top: 20px;
+  svg {
+    cursor: pointer;
+    float: right;
+    margin-right: 2rem;
+  }
+`;
 
 export const ContentsFormStyle = styled.div`
-    display: grid;
-    grid-template-columns: 3fr 1fr;
-    text-align: left;
-    padding: 1rem;
-    + div{
-        margin-top: 20px;
-        border-top: 2.5px solid rgba(100, 0, 100, 0.5);
-    }
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  text-align: left;
+  padding: 1rem;
+  + div {
+    margin-top: 20px;
+    border-top: 2.5px solid rgba(100, 0, 100, 0.5);
+  }
 `;
 
 export const ContentsInnerStyle = styled.div`
-    text-align: left;
-    padding: 1rem;
-    + div{
-        margin-top: 10px;
-        border-top: 2.5px solid rgba(100, 0, 100, 0.5);
-        
-    }
+  text-align: left;
+  padding: 1rem;
+  + div {
+    margin-top: 10px;
+    border-top: 2.5px solid rgba(100, 0, 100, 0.5);
+  }
 `;
 
 export const ProfileInnerStyle = styled.div`
-    padding: 1rem;
+  padding: 1rem;
 `;
