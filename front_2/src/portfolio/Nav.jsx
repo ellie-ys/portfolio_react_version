eff49a5093628846b63a44489172e71d89120351
Ellie-YS <dev.ellie7@gmail.com>
2021-12-23 20:09:44 +0900

    211223 [Fix] Client: remove unnecessary import

--- front_2/src/portfolio/Nav.jsx
+++ front_2/src/portfolio/Nav.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
